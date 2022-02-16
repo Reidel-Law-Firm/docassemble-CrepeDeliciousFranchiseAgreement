aff58545bf723f43126f147be4394b64665f7745
Anonymous User <65858862+miabonardi@users.noreply.github.com>
2022-02-16 09:03:42 -0500

    updates, still in progess

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.CrepeDeliciousFranchiseAgreement',
-      version='1.1',
+      version='1.2',
       description=('Crepe Delicious Franchise Agreement'),
       long_description='# docassemble.CrepeDeliciousFranchiseAgreement\r\n\r\nCrepe Delicious Franchise Agreement\r\n\r\n## Author\r\n\r\nSchuyler "Rocky" Reidel\r\n\r\nadmin@docs.reidellawfirm.com\r\n\r\n',
       long_description_content_type='text/markdown',
